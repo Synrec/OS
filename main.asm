03b978aff9698c8b89f52b7091b31f240e4ad1b2
Synrec <kylestclr@gmail.com>
2024-06-02 02:23:55 -0400

    Flag message

--- main.asm
+++ main.asm
@@ -10,13 +10,55 @@ mov dh, 0
 mov cl, 2
 mov dl, [diskNum]
 int 0x13
-mov ah, 0x0e
-mov al, [0x7e00]
-int 0x10
+jc startErrorPrint1
+jnc startErrorPrint2
+startErrorPrint1:
+    mov bx, errorMsg1
+    jmp error1
+startErrorPrint2:
+    mov bx, errorMsg2
+    jmp error2
+continue:
+    mov ah, 0x0e
+    mov al, [0x7e00]
+    int 0x10
+    mov al, [0x7e01]
+    int 0x10
+    mov al, [0x7e02]
+    int 0x10
+    mov al, [0x7e03]
+    int 0x10
+    mov al, [0x7e04]
+    int 0x10
+    mov al, [0x7e05]
+    int 0x10
+    mov al, [0x7e06]
+    int 0x10
+    jmp exit
+error1:
+    mov ah, 0x0e
+    mov al, [bx]
+    int 0x10
+    inc bx
+    cmp al, 0
+    je continue
+    jmp error1
+error2:
+    mov ah, 0x0e
+    mov al, [bx]
+    int 0x10
+    inc bx
+    cmp al, 0
+    je continue
+    jmp error1
 
-jmp $
+exit:
+    jmp $
 diskNum: db 0
+errorMsg1: db "Carry FLag: 1", 0
+errorMsg2: db "Carry Flag: 0", 0
 times 510-($-$$) db 0
 db 0x55, 0xaa
 
-times 512 db 'A'
+db "Sector2", 0
+times 520-($-$$) db 0
