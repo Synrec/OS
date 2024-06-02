122ad17e3283638aad09b5dc2afc462d0e19e154
Synrec <kylestclr@gmail.com>
2024-06-01 20:52:39 -0400

    reverse num

--- main.asm
+++ main.asm
@@ -1,41 +1,42 @@
 org 0x7c00
 bits 16
 
-string:
-    times 10 db 0
-    
+mov bp, 0x8000
+mov sp, bp
+mov bh, '~'
+push bx
 
-mov bx, string
+jmp decimalNum
 
-loop:
+decimalNum:
     mov ah, 0
     int 0x16
+    mov bh, al
+    push bx
+    cmp al, '/'
+    je printOut
     mov ah, 0x0e
     int 0x10
-    mov [bx], al
-    inc bx
-    mov al, [bx]
-    cmp al, 0
-    je loop
+    jmp decimalNum
+
+printOut:
     mov ah, 0x0e
-    jmp toEnd
+    mov al, bh
+    cmp al, '/'
+    jne output
+    jmp continue
 
-end:
-    db " [COMPLETE]", 0
-
-toEnd:
-    mov bx, end
-    jmp print
-
-print:
-    mov al, [bx]
-    cmp al, 0
+continue:
+    cmp al, '~'
     je exit
+    pop bx
+    jmp printOut
+
+output:
+    cmp al, '~'
+    je continue
     int 0x10
-    inc bx
-    cmp al, 0
-    je exit
-    jmp print
+    jmp continue
 
 exit:
     jmp $
